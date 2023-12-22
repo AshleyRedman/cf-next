c1eab9e047d98864e5ce944bd1047c9e6219e344
Ashley Redman <ashley.redman@ajrsoftware.com>
2023-12-22 17:51:32 +0000

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>ASSH!!!!</p>
+      <p>ASSH!!!! FLAG SET</p>
     </main>
   )
 }
